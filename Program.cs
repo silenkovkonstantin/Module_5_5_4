b565c7707e8cef5eaf54809fb036cd992f1a74bd
silenkovkonstantin <silenkov@inbox.ru>
2023-01-29 11:30:43 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class MainClass
+{
+
+    static decimal Factorial(int x)
+    {
+        if (x == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return x * Factorial(x - 1);
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        decimal f = Factorial(20);
+        Console.WriteLine(f.ToString());
+    }
+}
